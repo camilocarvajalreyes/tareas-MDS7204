57822b1ec18356711a3cb757215c30e07872f78f
camilocarvajalreyes <camilo.carvajal3009@gmail.com>
2022-09-23 13:28:52 -0300

    update FP clases generales

--- unidad_1/tarea_1/filtros/filtro_particulas.py
+++ unidad_1/tarea_1/filtros/filtro_particulas.py
@@ -1,23 +1,23 @@
 import numpy as np
-
-class Distribution:
-    def __init__(self):
-        pass
-
-    def sample(self,N):
-        samples = None
-        return samples
+from scipy.stats import norm
 
 
 class SequentialImportanceSampling:
     def __init__(self,N):
+        """Inicialización de la clase
+        
+        Argumentos
+        ----------
+                
+            N: int
+                numero de particulas por paso
+                
+        """
+        self.N = N
         self.particle_sequence = []
         self.weights_sequence = []
-        self.N = N
-
-    def sample_particles(self,dist):
-        new_particles = dist.sample(self.N)
-        self.particle_sequence.append(new_particles)
+        self._observed = []
+        self._medias = []
 
     def update_weights(self,y_obs):
         last_weights = self.weights_sequence[-1]
@@ -30,44 +30,56 @@ class SequentialImportanceSampling:
             new_weights.append(new_w)
         new_weights_norm = np.array([w/sum_w for w in new_weights])
         self.weights_sequence.append(new_weights_norm)
-
-
-class ParticleFilter1D:
-    def __init__(self,mu_inicial,N):
-        """Inicialización de la clase
-        
-        Argumentos
-        ----------
-        
-            mu_inicial: Distribution instance
-                distribucion para el estado inicial
-                
-            N: int
-                numero de sampleos por paso
-                
-        """
-        self._observed = []
-        self._medias = []
-        self.SIS = SequentialImportanceSampling(N)
-
-        self.mu_inicial = mu_inicial
-
-        self.SIS.sample_particles(self.mu_inicial)
-        self.SIS.weights_sequence.append(np.ones(N)/N)
     
+    def sample_particles(self):
+        raise NotImplementedError
+
+
+class ParticleFilter1D(SequentialImportanceSampling):
     def observe(self,y_obs):
+        "Observa una realización de la variable observable Y"
         self._observed.append(y_obs)
 
-
     def iterar(self):
-        """Método que lleva a cabo un paso del algoritmo"""
-        new_dist = Distribution()  # inferir distribución
-
-        particulas = self.SIS.sample_particles(new_dist)
-        pesos = self.SIS.update_weights(self._observed[-1])
+        "Método que lleva a cabo un paso del algoritmo"
+        particulas = self.sample_particles()
+        pesos = self.update_weights(self._observed[-1])
         self._medias.append(np.average(particulas,weights=pesos))
     
     # métodos getter
     @property
     def medias(self):
         return np.array(self._medias)
+
+
+class ParticleFilter1DStochasticVolatility(ParticleFilter1D):
+    """Clase que hereda de un filtro de particulas genérico (ParticleFilter1D)
+    
+    Específico para un modelo de volatilidad estocástica"""
+    def set_initial_particles(self,mu,sigma):
+        """
+        Samplea segun una distribución normal N(mu,sigma)
+        
+        Arguments
+        ----------
+        
+            mu: float
+                media de la normal a considerar
+
+            sigma: float
+                desviacion estandar (sigma^2 en estricto rigor)
+
+        """
+        self.particle_sequence.append(norm.rvs(mu,sigma,size=self.N))
+        self.weights_sequence.append(np.ones(self.N)/self.N)
+
+    @staticmethod
+    def sample_gaussian(mu,sigma):
+        new_particle = norm.rvs(mu,sigma,size=1)
+        return new_particle
+
+    def sample_particles(self):
+
+        new_particles = None
+
+        self.particle_sequence.append(new_particles)
