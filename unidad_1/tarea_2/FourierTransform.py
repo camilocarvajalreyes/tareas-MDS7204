23db3c41c258767953c6b873d71ac749b5d684d2
camilocarvajalreyes <camilo.carvajal3009@gmail.com>
2022-10-10 14:05:34 -0300

    update tarea 2 fft

--- unidad_1/tarea_2/FourierTransform.py
+++ unidad_1/tarea_2/FourierTransform.py
@@ -6,16 +6,40 @@ def DiscreteFourierTransform(x,k):
     return Xk
 
 
-def FastFourierTransform(x,N,s):
+def FastFourierTransform(x):
+    N = x.shape[0]
     X = np.zeros_like(x)
     if N==1:
-        return [x[0]]
+        X[0] = x
+        return X
     else:
-        half = int((N/2))
-        X[:half-1] = FastFourierTransform(x,half,2*s)
-        X[half-1:] = FastFourierTransform(x+s,half,2*s)
-        for k in range(half):
-            p = X[k]
-            q = np.exp(-2*k*np.pi*1j/N)*X[k+half]
-            X[k] = p + q
-            X[k+half] = p - q
+        X_par = FastFourierTransform(x[slice(0,len(x),2)])
+        X_impar = FastFourierTransform(x[slice(1,len(x),2)])
+        mitad = int((N/2))
+        X = np.concatenate(
+            (X_par+np.exp(-2j*np.pi*np.arange(mitad)/ N)*X_impar,
+            X_par+np.exp(-2j*np.pi*np.arange(mitad,N)/ N)*X_impar))
+        return X
+
+
+def FFT(x):
+    """
+    A recursive implementation of 
+    the 1D Cooley-Tukey FFT, the 
+    input should have a length of 
+    power of 2. 
+    """
+    N = len(x)
+    
+    if N == 1:
+        return x
+    else:
+        X_even = FFT(x[::2])
+        X_odd = FFT(x[1::2])
+        factor = \
+          np.exp(-2j*np.pi*np.arange(N)/ N)
+        
+        X = np.concatenate(\
+            [X_even+factor[:int(N/2)]*X_odd,
+             X_even+factor[int(N/2):]*X_odd])
+        return X
