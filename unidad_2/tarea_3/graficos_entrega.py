f305575b466e0d20ef08e3da21e0a74ead9f99ce
camilocarvajalreyes <camilo.carvajal3009@gmail.com>
2022-11-17 01:40:20 -0300

    updte tarea 3 indices gp

--- unidad_2/tarea_3/graficos_entrega.py
+++ unidad_2/tarea_3/graficos_entrega.py
@@ -1,6 +1,6 @@
 import numpy as np
 np.random.seed(0)
-from plot import plot_series, plot_data, plot_posterior
+from plot import plot_posterior
 from gp.gp_lite_tarea import gp_lite
 
 # Parámetros del código
@@ -12,8 +12,6 @@ img_dir = "unidad_2/tarea_3/informe/img"  # directorio en caso de guardar las fi
 
 # heart rates
 hr1 = np.loadtxt('unidad_1/tarea_2/hr.7257.txt')
-times = np.linspace(0,100,len(hr1))
-# hr2 = np.loadtxt('unidad_1/tarea_2/hr.11839.txt')
 
 # eliminando parte de las observaciones
 ALPHA = .3
@@ -27,31 +25,30 @@ test_ind = np.array([i for i in range(len(hr1)) if i not in indices])
 
 gp = gp_lite()
 gp.init_hypers()
+gp.show_hypers()
 
-i = len(indices)
-gp.compute_posterior(dimension=i)
 
-gp.sample(how_many=2)
+# gp.sample(how_many=2)
 # gp.plot_samples()
 
-gp.load(times[indices],hr1[indices])
+gp.load(indices,hr1[indices])
 # plot_data(gp)
 
-gp.compute_posterior(dimension=1000)
-# gp.plot_posterior(5,v_axis_lims = 35)
+gp.compute_posterior(where=np.arange(len(hr1)))
+
 print(f'negative log-likelihood modelo sin entrenar: {gp.nll()}')
 titulo = "Posterior para GP sin entrenar, alpha={}%".format(ALPHA*100)
 img_file = "untrained_gp_post.png" if save_plots else None
-plot_posterior(gp,0, test_points=hr1[test_ind], test_times=times[test_ind],title=titulo,save=img_file,folder=img_dir)
+plot_posterior(gp,0, test_points=hr1[test_ind], test_times=test_ind,title=titulo,save=img_file,folder=img_dir)
 
 
 # Entrenamiento
 if entrenar:
     gp.train()
     titulo = "Posterior para GP entrenada, alpha={}%".format(ALPHA*100)
-    gp.compute_posterior(dimension=1000)
+    gp.compute_posterior(where=np.arange(len(hr1)))
 
-    # gp.plot_posterior(5,v_axis_lims = 35)
     print(f'Negative log-likelihood modelo entrenado: {gp.nll()}')
+    gp.show_hypers()
     img_file = "trained_gp_post.png" if save_plots else None
-    plot_posterior(gp,0, test_points=hr1[test_ind],test_times=times[test_ind],save=img_file,folder=img_dir,title=titulo)
+    plot_posterior(gp,0, test_points=hr1[test_ind],test_times=test_ind,save=img_file,folder=img_dir,title=titulo)
