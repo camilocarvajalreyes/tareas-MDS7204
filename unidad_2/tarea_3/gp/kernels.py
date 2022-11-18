1e417c0d068f0cff481dfad820a3f5cb3540fab0
camilocarvajalreyes <camilo.carvajal3009@gmail.com>
2022-11-18 20:44:45 -0300

    updte periodic kernel gradient

--- unidad_2/tarea_3/gp/kernels.py
+++ unidad_2/tarea_3/gp/kernels.py
@@ -87,7 +87,7 @@ class SpectralMixtureKernel:
         return new_sigma_n, np.array([self.sigma, self.mu, self.gamma])
 
 class PeriodicKernel:
-    """Kernel Mixtura Espectral"""
+    """Kernel Periódico"""
     def __init__(self,sigma=1.0,length_scale=1.0,periodicity=1.0):
         self.sigma = sigma
         self.length_scale = length_scale
@@ -100,11 +100,11 @@ class PeriodicKernel:
         print(f'\tperiodicidad = {self.periodicity}')
     
     def __call__(self, X, Y):
-        return self.sigma**2 * np.exp(-2 * (np.sin(np.pi / self.periodicity * outersum(X,-Y)) / self.length_scale) ** 2)
+        return self.sigma**2 * np.exp(-2 * (np.sin(np.pi / self.periodicity * outersum(X,-Y)) / self.length_scale**2) ** 2)
     
     @staticmethod
     def kernel_eval(x,y,sigma,length_scale,periodicity):
-        return sigma**2 * np.exp (-2 * (np.sin(np.pi / periodicity * outersum(x,-y)) / length_scale) ** 2 )
+        return sigma**2 * np.exp (-2 * (np.sin(np.pi / periodicity * outersum(x,-y)) / length_scale**2) ** 2 )
     
     def nlogp(self,x,y,Nx,hypers):
         sigma = np.exp(hypers[0])
@@ -131,7 +131,7 @@ class PeriodicKernel:
         arg = np.pi * outersum(x,-x) / periodicity
         sin, cos = np.sin(arg), np.cos(arg)
 
-        dKdsigma = 2 * sigma * K
+        dKdsigma = 2*Gram/sigma
         dKdl = 4 * K * sin**2 / length_scale**3
         dKdperiod = 4 * K * sin / length_scale**2 * cos * arg / periodicity
         dKdsigma_n = 2*sigma_n*np.eye(Nx)
@@ -142,6 +142,72 @@ class PeriodicKernel:
         dlogp_dperiod = periodicity * 0.5*np.trace(H@dKdperiod)
         dlogp_dsigma_n = sigma_n * 0.5*np.trace(H@dKdsigma_n)
         return np.array([-dlogp_dsigma, -dlogp_dl, -dlogp_dperiod, -dlogp_dsigma_n])
+    
+    
+    def update_params(self, X, Y, sigma_n, Nx, verbose=True):
+		
+        hypers0 = np.array([np.log(self.sigma), np.log(self.length_scale), np.log(self.periodicity), np.log(sigma_n)])
+
+        jacobian = lambda hypers: self.dnlogp(X, Y, Nx, hypers)
+        obj_fun = lambda hypers: self.nlogp(X, Y, Nx, hypers)
+
+        res = minimize(obj_fun, hypers0, args=(), method='Newton-CG', jac=jacobian, options={'maxiter': 500, 'disp': verbose})
+
+        self.sigma = np.exp(res.x[0])
+        self.length_scale = np.exp(res.x[1])
+        self.periodicity = np.exp(res.x[2])
+        new_sigma_n = np.exp(res.x[3])
+
+        return new_sigma_n, np.array([self.sigma, self.length_scale, self.periodicity])
+
+
+class RBFKernel:
+    """Radial Basis Function kernel"""
+    def __init__(self,sigma=1.0,length_scale=1.0,periodicity=1.0):
+        self.sigma = sigma
+        self.length_scale = length_scale
+    
+    def show_hypers(self):
+        print('Hyperparámetros Kernel RBF:')
+        print(f'\tsigma = {self.sigma}')
+        print(f'\tlargo de escala = {self.length_scale}')
+    
+    def __call__(self, X, Y):
+        return self.sigma**2 * np.exp(-0.5 * (outersum(X,-Y) / self.length_scale)**2)
+    
+    @staticmethod
+    def kernel_eval(x,y,sigma,length_scale):
+        return sigma**2 * np.exp(-0.5 * (outersum(x,-y) / length_scale)**2)
+    
+    def nlogp(self,x,y,Nx,hypers):
+        sigma = np.exp(hypers[0])
+        length_scale = np.exp(hypers[1])
+        sigma_n = np.exp(hypers[2])
+
+        Gram = self.kernel_eval(x,x,sigma,length_scale)
+        K = Gram + sigma_n**2*np.eye(Nx) + 1e-5*np.eye(Nx)
+        (_, logdet) = np.linalg.slogdet(K)
+        return 0.5*(y.T@np.linalg.solve(K,y) + logdet + Nx*np.log(2*np.pi))
+
+    def dnlogp(self,x,y,Nx, hypers):
+        sigma = np.exp(hypers[0])
+        length_scale = np.exp(hypers[1])
+        sigma_n = np.exp(hypers[2])
+
+        Gram = self.kernel_eval(x,x,sigma,length_scale)
+        K = Gram + sigma_n**2*np.eye(Nx) + 1e-5*np.eye(Nx)
+
+        h = np.linalg.solve(K,y).T
+
+        dKdsigma = 2 * sigma * K
+        # dKdl = 4 * K * sin**2 / length_scale**3
+        dKdsigma_n = 2*sigma_n*np.eye(Nx)
+
+        H = (np.outer(h,h) - np.linalg.inv(K))
+        dlogp_dsigma = length_scale * 0.5*np.trace(H@dKdsigma)
+        # dlogp_dl = length_scale * 0.5*np.trace(H@dKdl)
+        dlogp_dsigma_n = sigma_n * 0.5*np.trace(H@dKdsigma_n)
+        # return np.array([-dlogp_dsigma, -dlogp_dl, -dlogp_dsigma_n])
     
     
     def update_params(self, X, Y, sigma_n, Nx, verbose=True):
